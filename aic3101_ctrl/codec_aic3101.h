18d8ba976c03204f0b10e93ea53c53dcee4b6385
Eric Brombaugh <ebrombaugh1@cox.net>
2025-03-21 16:27:04 -0700

    remove useless includes

--- aic3101_ctrl/codec_aic3101.h
+++ aic3101_ctrl/codec_aic3101.h
@@ -5,12 +5,6 @@
 #ifndef __codec_aic3101__
 #define __codec_aic3101__
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <getopt.h>
-#include <errno.h>
-#include "codec_aic3101.h"
-
 int codec_aic3101(int v, int b);
 
 #endif
